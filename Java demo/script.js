890637e5808a975443f446911bad8edf820d0d57
Hamed Heydari <Hamedheydari0137@gmail.com>
2025-04-25 14:01:41 +0330

    Added Java demo

---
+++ Java demo/script.js
@@ -0,0 +1,26 @@
+var firstname="Hamed";
+var lastname="Heydari";
+var age="29";
+var height="1.83";
+var weight="81";
+var job="Front-End Developer";
+
+
+console.log (firstname);
+console.log (lastname);
+console.log (age);
+console.log (height);
+console.log (weight);
+console.log (job);
+
+var x=18.25;
+var y=16;
+var average=(x+y) /2;
+
+console.log (average);
+
+var midterm=18.25;
+var final=16;
+var average= (midterm+final) /2;
+
+console.log (average);
