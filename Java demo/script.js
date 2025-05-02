2f357490aa3ec58aee873d99f1ecfb959e54b2fe
Hamed Heydari <Hamedheydari0137@gmail.com>
2025-05-02 13:35:02 +0330

    Added 6 exercises

--- Java demo/script.js
+++ Java demo/script.js
@@ -36,6 +36,95 @@ if (average>= 10) {
 console.log ('end of the script');
 
 
+var number=3;
+if (number ==2) {
+    console.log ("even", 2);
+}  else if (number==4) {
+    console.log ("even", 4);
+}  else if (number==1) {
+    console.log ("odd", 1);
+}  else if (number===3) {
+    console.log ("odd", 3);
+}  else if (number==5) {
+    console.log ("odd", 5);
+}  else {
+    console.log("end of the script");
+}
 
+
+var number=7;
+console.log ("the number is:", number)
+if (number>0) {
+    console.log ("the number is positive.");
+} else if (number<0) {
+    console.log ("the number is negative");
+} else {
+    console.log ("the number is zero");
+}
+
+console.log ("end of the script");
+
+
+var x=6;
+var y=13;
+
+
+if (x>y) {
+    console.log (x,"larger");
+} else if (y>x) {
+    console.log (y, "larger");
+} else {
+    console.log ("Both numbers are equal");
+}
+
+console.log ("end of the script");
+
+
+var score=75;
+
+if (score>= 90 && score<=100) {
+    console.log ("A");
+} else if (score>=80 && score<=89) {
+    console.log ("B");
+} else if (score >=70 && score<=79) {
+    console.log ("C"); 
+} else if (score>=60 && score<=69) {
+    console.log ("D");
+} else if (score>= 0 && score<60) {
+    console.log ("E");
+} else {
+    console.log ("Invalid score");
+}
+
+console.log ("End of the script");
+
+
+var year=2024;
+
+
+if (((year / 4)* 4 == year && (year /100)* 100 != year)|| (year/400)*400==year) {
+   console.log (year + " is a leap year");
+} else {
+    console.log (year + "is not a leap year");
+}
+
+console.log ("end of the script");
+
+
+
+
+var correctUsername= "admin";
+var correctPassword= "1234";
+
+var username= "admin";
+var password= "1234";
+
+if (username ===correctUsername && password === correctPassword) {
+    console.log ("Access granted");
+} else {
+    console.log ("Access denied");
+}
+
+console.log ("end of the script");
 
 
